d0f679e9f1be1e8c839547b1e8ad87158132382d
divyansh630 <divyanshk6767@gmail.com>
2026-08-24 10:21:46 +0530

    props in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { User } from "./components/user";
 import { Header } from "./components/header";
+import { Userinfo } from "./props/userinfo";
 function App() {
   return (
     <div>
+      <Userinfo name="Alex" age={20} />
       <Header />
       <User />
     </div>
---
+++ src/props/userinfo.jsx
@@ -0,0 +1,8 @@
+export const Userinfo = ({ name, age }) => {
+  return (
+    <>
+      <h2>My Name is {name}</h2>
+      <p>and age is {age}</p>
+    </>
+  );
+};
